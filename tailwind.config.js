494ec263536be666439bdb61b7867f77b5990d70
Krishna Singhal <krishna@Krishnas-MacBook-Air.local>
2025-05-18 18:13:47 +0530

    edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class', // 👈 ADD THIS LINE
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
